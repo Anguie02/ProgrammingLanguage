48c96884ebda4fa33413a6a4638bbc94e411fb85
Anguie02 <1487186@senati.pe>
2024-04-02 23:02:20 -0500

    Implementación de funcionalidad para buscar y filtrar

---
+++ SistemaAlmacen/Entregable2/SistemaAlmacen/FormFiltrarcs.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SistemaAlmacen
+{
+    partial class FormFiltrarcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFiltrarcs));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboFiltrar = new System.Windows.Forms.ComboBox();
+            this.txtFiltrar = new System.Windows.Forms.TextBox();
+            this.panelListar = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.comboFiltrar);
+            this.panel1.Controls.Add(this.txtFiltrar);
+            this.panel1.Location = new System.Drawing.Point(39, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(692, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnBuscar.Location = new System.Drawing.Point(397, 31);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(97, 37);
+            this.btnBuscar.TabIndex = 19;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(565, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboFiltrar
+            // 
+            this.comboFiltrar.BackColor = System.Drawing.Color.SteelBlue;
+            this.comboFiltrar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboFiltrar.FormattingEnabled = true;
+            this.comboFiltrar.Items.AddRange(new object[] {
+            "Nombre",
+            "Categoría"});
+            this.comboFiltrar.Location = new System.Drawing.Point(551, 54);
+            this.comboFiltrar.Name = "comboFiltrar";
+            this.comboFiltrar.Size = new System.Drawing.Size(102, 24);
+            this.comboFiltrar.TabIndex = 1;
+            this.comboFiltrar.Text = "Filtrar por:";
+            // 
+            // txtFiltrar
+            // 
+            this.txtFiltrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFiltrar.Location = new System.Drawing.Point(30, 35);
+            this.txtFiltrar.Name = "txtFiltrar";
+            this.txtFiltrar.Size = new System.Drawing.Size(343, 26);
+            this.txtFiltrar.TabIndex = 0;
+            // 
+            // panelListar
+            // 
+            this.panelListar.BackColor = System.Drawing.Color.SteelBlue;
+            this.panelListar.Location = new System.Drawing.Point(39, 140);
+            this.panelListar.Name = "panelListar";
+            this.panelListar.Size = new System.Drawing.Size(692, 242);
+            this.panelListar.TabIndex = 1;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Teal;
+            this.btnSalir.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(684, 400);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(104, 42);
+            this.btnSalir.TabIndex = 16;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            // 
+            // FormFiltrarcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.panelListar);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormFiltrarcs";
+            this.Text = "FormFiltrarcs";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtFiltrar;
+        private System.Windows.Forms.Panel panelListar;
+        private System.Windows.Forms.ComboBox comboFiltrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ SistemaAlmacen/Entregable2/SistemaAlmacen/FormFiltrarcs.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaAlmacen
+{
+    public partial class FormFiltrarcs : Form
+    {
+        // Cadena de conexión a la base de datos
+        string connectionString = "Data Source=(localdb)\\senati;Initial Catalog=logins;Integrated Security=True";
+
+        public FormFiltrarcs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            // Obtener el texto del campo de filtro
+            string filtro = txtFiltrar.Text;
+
+            // Determinar el tipo de búsqueda seleccionado por el usuario en el ComboBox
+            string tipoBusqueda = comboFiltrar.SelectedItem?.ToString();
+
+            // Si el usuario no ha seleccionado ningún tipo de búsqueda, se asume búsqueda por nombre
+            if (string.IsNullOrEmpty(tipoBusqueda))
+            {
+                tipoBusqueda = "Nombre";
+            }
+
+            try
+            {
+                // Crear la conexión
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Crear la consulta SQL
+                    string query = "";
+
+                    // Construir la consulta según el tipo de búsqueda seleccionado
+                    if (tipoBusqueda == "Nombre")
+                    {
+                        query = @"SELECT * FROM Productos WHERE nombre LIKE @Filtro";
+                    }
+                    else if (tipoBusqueda == "Categoría")
+                    {
+                        query = @"SELECT * FROM Productos WHERE categoria LIKE @Filtro";
+                    }
+
+                    // Crear el comando con la consulta SQL y la conexión
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Agregar el parámetro para el filtro
+                        command.Parameters.AddWithValue("@Filtro", "%" + filtro + "%");
+
+                        // Abrir la conexión
+                        connection.Open();
+
+                        // Crear un lector de datos para leer los resultados
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Limpiar el panel antes de agregar nuevos elementos
+                            panelListar.Controls.Clear();
+
+                            // Recorrer los resultados y agregarlos al panel
+                            while (reader.Read())
+                            {
+                                // Crear un nuevo control para mostrar los datos
+                                Label label = new Label();
+                                label.Text = $"Nombre: {reader["Nombre"]}, Descripción: {reader["Descripcion"]}, SKU: {reader["SKU"]}, Categoría: {reader["Categoria"]}, Precio: {reader["Precio"]}, Cantidad en stock: {reader["CantidadEnStock"]}, Unidad de medida: {reader["UnidadMedida"]}, Fecha de vencimiento: {reader["FechaVencimiento"]}";
+
+                                // Añadir el control al panel
+                                panelListar.Controls.Add(label);
+                            }
+                        }
+
+                        // Cerrar la conexión
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+    
+}
